d1ae9c8e0c326de7097d5b917c3db5fa689f684e
Drkymrn <97692237+Drkymrn@users.noreply.github.com>
2022-02-04 11:59:10 -0500

    Added Comments to script file

--- Derek M. & Tyler R.'s project/Assets/PlayerController.cs
+++ Derek M. & Tyler R.'s project/Assets/PlayerController.cs
@@ -7,12 +7,13 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Idk I Love Black...
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //This is a comment
+        /* This is also a comment */
     }
 }
