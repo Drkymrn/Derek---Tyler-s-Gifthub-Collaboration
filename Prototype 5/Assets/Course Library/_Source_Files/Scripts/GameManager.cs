a1ee140b27fbeb75671f7ca14aabf93dbc9494cd
Drkymrn <97692237+Drkymrn@users.noreply.github.com>
2022-04-13 11:55:25 -0400

    Prototype 5.2

--- Prototype 5/Assets/Course Library/_Source_Files/Scripts/GameManager.cs
+++ Prototype 5/Assets/Course Library/_Source_Files/Scripts/GameManager.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     //public GameObjects[] targets2;
     private float spawnRate = 1.0f;
+    private int score;
+    public TextMeshProUGUI scoreText;
+   
+   
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
     }
 
     // Update is called once per frame
@@ -26,6 +32,12 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
+            UpdateScore(5);
         }
+    }
+    public void UpdateScore(int ScoreToAdd)
+    {
+        score += ScoreToAdd;
+        scoreText.text = "Score: " + score;
     }
 }
--- Prototype 5/Assets/Course Library/_Source_Files/Scripts/Target.cs
+++ Prototype 5/Assets/Course Library/_Source_Files/Scripts/Target.cs
@@ -10,6 +10,9 @@ public class Target : MonoBehaviour
     private float xRange = 4;
     private float ySpawnPos = -6;
     private Rigidbody targetRb;
+    private GameManager gameManager;
+     public int pointValue;
+     public ParticalSystem explosionParticle;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +20,7 @@ public class Target : MonoBehaviour
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
         transform.position = RandomSpawnPos();
-
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
 
     }
         Vector3 RandomForce()
@@ -40,6 +43,9 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
+
     }
     private void OntriggerEnter(Collider other)
     {
